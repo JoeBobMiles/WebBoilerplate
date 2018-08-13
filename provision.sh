42b2b547f7d7fe5c71c4d19004d40f458fbb48e0
Joseph Miles <josephmiles2015@gmail.com>
2018-08-13 17:48:59 -0600

    Base is working, now just need to setup routing.

--- provision.sh
+++ provision.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
+#
 # provision.sh
+#
 # Unlike other source files in this repository, this file uses tabs instead of
-# spaces since bash scripts are annoying and rely on tabs characters in order
-# for certain formating tricks to work (looking at you '<<-').
+# spaces. The reason is because bash scripts rely on tabs characters for
+# certain formating tricks to work (looking at you '<<-').
 
-# force exit on error so that we don't waste time.
+# force exit on error so that we don't waste time if something goes wrong.
 set -e
 
-# update the base box.
-sudo apt-get update -y
+apt-get update -y
 
-# install nginx
-sudo apt-get install nginx
+MYSQL_PASSWORD='root'
 
-# install mysql
-sudo apt-get install -y mysql-server
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD"
+apt-get install -y nginx mysql-server
 
-# perform secure installation steps
-mysql -uroot <<-MYSQL
-	update mysql.user set password = password('root') where User = 'root';
-	delete from mysql.user where User = '';
-	delete from mysql.user where
-		User = 'root' and
-		Host not in ('localhost', '127.0.0.1', '::1');
-	drop database test;
-	delete from mysql.db where Db = 'test' or Db = 'test\_%';
-	flush privileges;
-MYSQL
-
-# install php (7.0 for now, we'll deal with the remi repository later)
+# installing php 7.0 for now, we'll deal with the remi repository later
 sudo apt-get install -y php-fpm php-mysql
 
 # uncomment cgi.fix_path_info and set it to false
+sed -Ein 's/^;(cgi\.fix_pathinfo).*/\1=0/' /etc/php/7.0/fpm/php.ini
 
 # change out nginx configuration
+cp /vagrant/nginx-config /etc/nginx/sites-available/default
+service nginx reload
 
-# setup symbolic link between /vagrant and /var/www/html (or whatever our
+# setup symbolic link between /vagrant and /var/www (or whatever our
 # target hosting directory is).
+rm -rf /var/www
+ln -s /vagrant /var/www
